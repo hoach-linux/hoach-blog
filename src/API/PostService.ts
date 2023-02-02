7b5e2d70a366772d2b007205e6e12d45ba3c1607
hoach-linux <hoachprogrammer@gmail.com>
2023-02-02 12:49:48 +0700

    fixing bug

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 
 export default class PostService {
   static async getAll() {
+    const apiKey = "0182127d10ac471ba19b9ca744430175";
     const response = await axios.get("https://newsapi.org/v2/top-headlines", {
       params: {
         category: "technology",
         sortBy: "popularity",
-        apiKey: "0182127d10ac471ba19b9ca744430175",
+      },
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
       },
     });
 
