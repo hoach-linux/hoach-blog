eba6a63bc9e845a0e470b1dcc95be4a1f02f478f
hoach-linux <hoachprogrammer@gmail.com>
2023-02-07 22:52:42 +0700

    new component

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 import PostList from "../components/PostList";
-import { useState } from "react";
+import { Button } from "@mui/joy";
+
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
   const favoritesPosts: any = localStorage.getItem("favoritesPosts");
-  const posts = JSON.parse(favoritesPosts);
-  
+  let posts: any[] = [];
+
+  if (JSON.parse(favoritesPosts)) {
+    posts = JSON.parse(favoritesPosts);
+  } else {
+    posts = [];
+  }
+
   return (
     <div>
-      <PostList posts={posts} title="Favorites" />
+      {posts.length ? (
+        <PostList posts={posts} title="Favorites" />
+      ) : (
+        <Link to="/">
+          <Button size="lg" href="/" fullWidth>
+            Add favorites
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
